55b1d979b91aeec51f982efc1221e64f9fe3d8fd
Meda Serenaite <meda.serenaite@gitlab.com>
2020-05-18 14:05:59 -0500

    adds more changes and stuff

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,9 +2,12 @@
 
 // Put JS logic (if any) here.
 // ESNext to ES5 transpiling is completed via Babel's "env" preset.
+console.log('load');
 var burgerMenu = document.querySelector(".nav__burger");
-var expandedMobileMenu = document.querySelector(".nav__list");
+var expandedMobileMenu = document.querySelector(".nav__list ul");
 burgerMenu.addEventListener('click', function () {
+  console.log('click');
+
   if (expandedMobileMenu.style.display === "block") {
     expandedMobileMenu.style.display = "none";
   } else {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
 // Put JS logic (if any) here.
 // ESNext to ES5 transpiling is completed via Babel's "env" preset.
 
+console.log('load')
 let burgerMenu = document.querySelector(".nav__burger")
-let expandedMobileMenu = document.querySelector(".nav__list")
+let expandedMobileMenu = document.querySelector(".nav__list ul")
 
 burgerMenu.addEventListener('click', function() {
-
+console.log('click')
     if(expandedMobileMenu.style.display === "block"){
         expandedMobileMenu.style.display = "none"
     } else {
         expandedMobileMenu.style.display = "block"
+
     }
 })
