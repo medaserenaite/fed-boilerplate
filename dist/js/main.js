c3afcccfeb5d8ca1d583340c45a6b8b177923773
Meda Serenaite <meda.serenaite@gitlab.com>
2020-05-19 16:54:30 -0500

    more banner things

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,20 +16,30 @@
 var desktop = document.getElementById("desktopOnly");
 var mobile = document.getElementById("mobileOnly");
 var desktopMobile = document.getElementById("desktopMobile");
-var background = document.getElementById("background"); // if(desktop.checked = "true"){
-//     console.log('hi')
-// }
-// function toggleCheck() {
-//     if(desktop.checked = "true"){
-//         desktop.checked = false
-//     }
-// }
+var background = document.getElementById("background");
+var eyebrow = document.querySelectorAll(".eyebrow");
+var eyebrowForm = document.getElementById("eyebrowForm"); // eyebrowToggle()
+// eyebrow.forEach((el, i) => {
+//     eyebrow[i].addEventListener('click', function () {
+// })
+// })
 
 function myFunction() {
   var components = document.querySelectorAll('.banner__container'); // console.log(components)
 
   components.forEach(function (el, i) {
     components[i].style.backgroundImage = "url('https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg')"; // components[i].style.backgroundSize = "60px 120px"
+  });
+}
+
+function bottomEl() {
+  var bottomEls = document.querySelectorAll('.cards__mobile');
+  bottomEls.forEach(function (el, i) {
+    var x = document.querySelectorAll('.cards');
+    x.forEach(function (xyz, j) {
+      x[0].style.display = 'flex';
+      x[1].style.display = 'block';
+    });
   });
 }
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,15 +19,21 @@ let desktop = document.getElementById("desktopOnly")
 let mobile = document.getElementById("mobileOnly")
 let desktopMobile = document.getElementById("desktopMobile")
 let background = document.getElementById("background")
-// if(desktop.checked = "true"){
-//     console.log('hi')
-// }
+let eyebrow = document.querySelectorAll(".eyebrow")
 
-// function toggleCheck() {
-//     if(desktop.checked = "true"){
-//         desktop.checked = false
-//     }
-// }
+
+let eyebrowForm = document.getElementById("eyebrowForm")
+
+
+// eyebrowToggle()
+// eyebrow.forEach((el, i) => {
+//     eyebrow[i].addEventListener('click', function () {
+        
+// })
+
+    
+// })
+
 
 function myFunction(){
     let components = document.querySelectorAll('.banner__container')
@@ -36,5 +42,16 @@ function myFunction(){
     components.forEach((el, i) => {
         components[i].style.backgroundImage = "url('https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg')";
         // components[i].style.backgroundSize = "60px 120px"
+    })
+}
+
+function bottomEl() {
+    let bottomEls = document.querySelectorAll('.cards__mobile')
+    bottomEls.forEach((el, i) => {
+        let x = document.querySelectorAll('.cards')
+        x.forEach((xyz, j) => {
+            x[0].style.display = 'flex'
+            x[1].style.display = 'block'
+        })
     })
 }
