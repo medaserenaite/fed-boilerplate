99a86c45e95e8f0aa1f4e2eff14935b19ea2f6d3
Meda Serenaite <meda.serenaite@gitlab.com>
2020-05-19 15:09:39 -0500

    banner things

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,16 +2,34 @@
 
 // Put JS logic (if any) here.
 // ESNext to ES5 transpiling is completed via Babel's "env" preset.
-console.log('load');
-var burgerMenu = document.querySelector(".nav__burger");
-var expandedMobileMenu = document.querySelector(".nav__list ul");
-burgerMenu.addEventListener('click', function () {
-  console.log('click');
+// console.log('load')
+// let burgerMenu = document.querySelector(".nav__burger")
+// let expandedMobileMenu = document.querySelector(".nav__list ul")
+// burgerMenu.addEventListener('click', function() {
+// console.log('click')
+//     if(expandedMobileMenu.style.display === "block"){
+//         expandedMobileMenu.style.display = "none"
+//     } else {
+//         expandedMobileMenu.style.display = "block"
+//     }
+// })
+var desktop = document.getElementById("desktopOnly");
+var mobile = document.getElementById("mobileOnly");
+var desktopMobile = document.getElementById("desktopMobile");
+var background = document.getElementById("background"); // if(desktop.checked = "true"){
+//     console.log('hi')
+// }
+// function toggleCheck() {
+//     if(desktop.checked = "true"){
+//         desktop.checked = false
+//     }
+// }
 
-  if (expandedMobileMenu.style.display === "block") {
-    expandedMobileMenu.style.display = "none";
-  } else {
-    expandedMobileMenu.style.display = "block";
-  }
-});
+function myFunction() {
+  var components = document.querySelectorAll('.banner__container'); // console.log(components)
+
+  components.forEach(function (el, i) {
+    components[i].style.backgroundImage = "url('https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg')"; // components[i].style.backgroundSize = "60px 120px"
+  });
+}
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,40 @@
 // Put JS logic (if any) here.
 // ESNext to ES5 transpiling is completed via Babel's "env" preset.
 
-console.log('load')
-let burgerMenu = document.querySelector(".nav__burger")
-let expandedMobileMenu = document.querySelector(".nav__list ul")
+// console.log('load')
+// let burgerMenu = document.querySelector(".nav__burger")
+// let expandedMobileMenu = document.querySelector(".nav__list ul")
 
-burgerMenu.addEventListener('click', function() {
-console.log('click')
-    if(expandedMobileMenu.style.display === "block"){
-        expandedMobileMenu.style.display = "none"
-    } else {
-        expandedMobileMenu.style.display = "block"
+// burgerMenu.addEventListener('click', function() {
+// console.log('click')
+//     if(expandedMobileMenu.style.display === "block"){
+//         expandedMobileMenu.style.display = "none"
+//     } else {
+//         expandedMobileMenu.style.display = "block"
 
-    }
-})
+//     }
+// })
+
+let desktop = document.getElementById("desktopOnly")
+let mobile = document.getElementById("mobileOnly")
+let desktopMobile = document.getElementById("desktopMobile")
+let background = document.getElementById("background")
+// if(desktop.checked = "true"){
+//     console.log('hi')
+// }
+
+// function toggleCheck() {
+//     if(desktop.checked = "true"){
+//         desktop.checked = false
+//     }
+// }
+
+function myFunction(){
+    let components = document.querySelectorAll('.banner__container')
+    // console.log(components)
+
+    components.forEach((el, i) => {
+        components[i].style.backgroundImage = "url('https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg')";
+        // components[i].style.backgroundSize = "60px 120px"
+    })
+}
